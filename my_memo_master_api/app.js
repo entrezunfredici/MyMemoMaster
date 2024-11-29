f564eaa42f1502c312ddd2c2c350544b81aed81e
LalbaAnthony <anthonylalaba24@gmail.com>
2024-11-29 20:54:29 +0100

    [FIX] Fix de swagger

--- my_memo_master_api/app.js
+++ my_memo_master_api/app.js
@@ -26,24 +26,7 @@ app.use(bodyParser.json());
 app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // Middleware pour servir la documentation Swagger
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API Documentation',
-            version: '1.0.0',
-            description: 'Documentation de l\'API avec OpenAPI et Swagger',
-        },
-        servers: [
-            {
-                url: 'http://localhost:' + process.env.API_PORT,
-            },
-        ],
-    },
-    apis: ['./routers/*.js'], // Chemin des fichiers de routes
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
+const swaggerSpec = swaggerJsdoc(require('./swagger.config.js'));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes
