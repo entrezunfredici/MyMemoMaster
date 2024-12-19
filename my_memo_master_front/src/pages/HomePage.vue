b0f1eed291a346f830826ec68155773085e04004
LalbaAnthony <anthonylalaba24@gmail.com>
2024-12-19 01:39:46 +0100

    Resolve merge confilcts

--- my_memo_master_front/src/pages/HomePage.vue
+++ my_memo_master_front/src/pages/HomePage.vue
@@ -1,64 +1,130 @@
 <template>
   <div>
-<<<<<<< HEAD
     <section>
       Salut salut
     </section>
+
     <section>
       <DiagramTest />
     </section>
-=======
     Salut salut
 
-    <Dropdown name="Trier" side="right">
-      Salut salut
-    </Dropdown>
+    <section>
+      <Dropdown name="Trier" side="right">
+        Salut salut
+      </Dropdown>
 
-    <Button>Tesst</Button>
+      <Button>Tesst</Button>
+    </section>
 
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero accusamus itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
->>>>>>> 69ebf20e338ed7268a3719a33f7f194750a81668
+    <section>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut dolore, omnis iure voluptatem libero
+        accusamus
+        itaque sequi optio? Iste, fugiat. Quis corporis nemo quibusdam iusto, saepe neque excepturi vel.</p>
+    </section>
+
   </div>
 </template>
 
 <script setup>
-<<<<<<< HEAD
 import DiagramTest from '@/components/DiagramTestComponent.vue'
-=======
 import Button from '@/components/ButtonComponent.vue'
 import Dropdown from '@/components/DropdownComponent.vue'
->>>>>>> 69ebf20e338ed7268a3719a33f7f194750a81668
 
 </script>
