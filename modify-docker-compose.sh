1276ef51d09be96b1f79b2077da297946efe753b
entrezunfredici <superfred2468@gmail.com>
2025-01-01 12:38:27 +0100

    [IMP] docker modifier

--- modify-docker-compose.sh
+++ modify-docker-compose.sh
@@ -5,3 +5,7 @@ DOCKER_COMPOSE_FILE="/var/www/html/mymemomaster/docker-compose.yml"
 
 # Supprimer les services postgres et pgadmin du fichier docker-compose.yml
 sed -i '/postgres:/,/pgadmin:/d' $DOCKER_COMPOSE_FILE
+
+# Supprimer les volumes postgres-data et pgadmin-data du fichier docker-compose.yml
+sed -i '/postgres-data:/d' $DOCKER_COMPOSE_FILE
+sed -i '/pgadmin-data:/d' $DOCKER_COMPOSE_FILE
