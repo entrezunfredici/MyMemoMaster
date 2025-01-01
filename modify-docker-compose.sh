3abecb0fe5c6de3befe5962e0b49812c2d7c3734
entrezunfredici <superfred2468@gmail.com>
2025-01-01 15:16:47 +0100

    [FIX] deploy error part 3

--- modify-docker-compose.sh
+++ modify-docker-compose.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
-# Chemin vers le fichier docker-compose.yml
 DOCKER_COMPOSE_FILE="/var/www/html/mymemomaster/docker-compose.yml"
 
-# Supprimer les services postgres et pgadmin du fichier docker-compose.yml
-sed -i '/postgres:/,/pgadmin:/d' $DOCKER_COMPOSE_FILE
+if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
+    echo "Le fichier $DOCKER_COMPOSE_FILE n'existe pas. Abandon."
+    exit 1
+fi
 
-# Supprimer les volumes postgres-data et pgadmin-data du fichier docker-compose.yml
-sed -i '/postgres-data:/d' $DOCKER_COMPOSE_FILE
-sed -i '/pgadmin-data:/d' $DOCKER_COMPOSE_FILE
+sed -i '/^  postgres:/,/^[^ ]/d' "$DOCKER_COMPOSE_FILE"
+sed -i '/^  pgadmin:/,/^[^ ]/d' "$DOCKER_COMPOSE_FILE"
+
+sed -i '/^  postgres-data:/d' "$DOCKER_COMPOSE_FILE"
+sed -i '/^  pgadmin-data:/d' "$DOCKER_COMPOSE_FILE"
+
+echo "$DOCKER_COMPOSE_FILE has editted."
