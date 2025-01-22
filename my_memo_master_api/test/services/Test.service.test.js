d1a27a6f95b546e993b94db9fa6bae4a4b36ab07
gaiailou <gaia.ducournau@gmail.com>
2025-01-22 15:53:53 +0100

    [FIX] Tests tests

--- my_memo_master_api/test/services/Test.service.test.js
+++ my_memo_master_api/test/services/Test.service.test.js
@@ -18,8 +18,8 @@ describe("TestService", () => {
   
     test("should retrieve all tests", async () => {
       const mockTests = [
-        { id: 1, subjectId: 1, name: "Controle milieu semestre" },
-        { id: 2, subjectId: 1, name: "Controle finale" },
+        { testId: 1, subjectId: 1, name: "Controle milieu semestre" },
+        { testId: 2, subjectId: 1, name: "Controle finale" },
       ];
       Test.findAll.mockResolvedValue(mockTests);
   
@@ -30,7 +30,7 @@ describe("TestService", () => {
     });
   
     test("should retrieve a test by ID", async () => {
-      const mockTest = { id: 1, subjectId: 1, name: "Controle finale" };
+      const mockTest = { testId: 1, subjectId: 1, name: "Controle finale" };
       Test.findByPk.mockResolvedValue(mockTest);
   
       const test = await TestService.findOne(1);
@@ -40,22 +40,22 @@ describe("TestService", () => {
     });
   
     test("should create a new test", async () => {
-      const mockTest = { id: 3, subjectId: 1, name: "Controle m1" };
+      const mockTest = { testId: 3, subjectId: 1, name: "Controle m1" };
       Test.create.mockResolvedValue(mockTest);
   
-      const test = await TestService.create({ subjectId: 1,name: "Controle m1" });
+      const test = await TestService.create({ subjectId: 1, name: "Controle m1" });
   
-      expect(Test.create).toHaveBeenCalledWith({ subjectId: 1,name: "Controle m1" });
+      expect(Test.create).toHaveBeenCalledWith({ subjectId: 1, name: "Controle m1" });
       expect(test).toEqual(mockTest);
     });
 
     test("should update a test", async () => {
-        const mockTest = { id: 1, subjectId: 1, name: "Controle mis à jour" };
+        const mockTest = { subjectId: 1, name: "Controle mis à jour" };
         Test.update.mockResolvedValue([1, [mockTest]]); // Update renvoie généralement un tableau [nombre de lignes modifiées, les objets mis à jour]
 
         const updatedTest = await TestService.update(1, { name: "Controle mis à jour" });
 
-        expect(Test.update).toHaveBeenCalledWith({ name: "Controle mis à jour" }, { where: { id: 1 } });
+        expect(Test.update).toHaveBeenCalledWith({ name: "Controle mis à jour" }, { where: { testId: 1 } });
         expect(updatedTest).toEqual(mockTest);
     });
 
@@ -63,9 +63,9 @@ describe("TestService", () => {
         const mockTest = { id: 1, subjectId: 1, name: "Controle à supprimer" };
         Test.destroy.mockResolvedValue(1); 
 
-        const deletedTest = await TestService.destroy(1);
+        const deletedTest = await TestService.delete(1);
 
-        expect(Test.destroy).toHaveBeenCalledWith({ where: { id: 1 } });
+        expect(Test.destroy).toHaveBeenCalledWith({ where: { testId: 1 } });
         expect(deletedTest).toBe(true);
     });
 
