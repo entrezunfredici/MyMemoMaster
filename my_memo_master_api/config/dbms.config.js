5a80680190eb4d53bc28f9813d3c04b83c597681
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-01-23 14:12:11 +0100

    [MAJ] Pipe

--- my_memo_master_api/config/dbms.config.js
+++ my_memo_master_api/config/dbms.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 module.exports = {
-  username: process.env.PG_USER || "postgres",
-  password: process.env.PG_PASS || "admin",
-  database: process.env.PG_DB || "mymemomasterdb",
-  host: process.env.PG_HOST || "localhost",
-  port: process.env.PG_PORT || 5432,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  database: process.env.PG_DB,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
   dialect: "postgres",
   logging: false,
 };
