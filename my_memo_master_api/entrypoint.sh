cec6330e652b220eaf0c02fc6edbcaa3fab02d98
entrezunfredici <superfred2468@gmail.com>
2025-03-26 15:14:46 +0100

    [DEL] comments

--- my_memo_master_api/entrypoint.sh
+++ my_memo_master_api/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-
-# Arrêter l'exécution en cas d'erreur
 set -e
 
-# Vérifier si node_modules est bien installé, sinon l'installer
 if [ ! -d "node_modules" ]; then
   echo "📦 Installing dependencies..."
   npm install
 fi
 
-# Exécuter les migrations et le seed (si nécessaire)
 echo "🌱 Running database seed..."
 npm run seed || echo "⚠️ Seed script failed, but continuing..."
 
-# Lancer l'application
 echo "🚀 Starting the API..."
 exec npm run start
