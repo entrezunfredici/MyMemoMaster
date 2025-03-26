f8f729639e48ac6ccf4e70f3a3f5bce6895f8a08
gaiailou <gaia.ducournau@gmail.com>
2025-03-26 14:49:52 +0100

    [ADD] test du store Role

--- my_memo_master_front/src/pages/SettingsPage.vue
+++ my_memo_master_front/src/pages/SettingsPage.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    Salut salut
+    Salut salut, ici c'est les paramètres
   </div>
+  <p v-if="roleStore.role.name">Rôle : {{ roleStore.role.name }}</p>
+  <p v-else>Chargement du rôle...</p>
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+import { useRoleStore } from '@/stores/role'
+
+const roleStore = useRoleStore()
+
+onMounted(async () => {
+  const userId = 1 
+  await roleStore.fetchRoleById(userId)
+})
 
 </script>
