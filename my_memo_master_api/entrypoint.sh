627269e99c5eaa84f49ae5b8b075a6fb14047f84
entrezunfredici <superfred2468@gmail.com>
2025-03-26 15:20:32 +0100

    [WIP] seeder pt

--- my_memo_master_api/entrypoint.sh
+++ my_memo_master_api/entrypoint.sh
@@ -5,9 +5,9 @@ if [ ! -d "node_modules" ]; then
   echo "📦 Installing dependencies..."
   npm install
 fi
-
-echo "🌱 Running database seed..."
-npm run seed || echo "⚠️ Seed script failed, but continuing..."
+#seed doent work
+#echo "🌱 Running database seed..."
+#npm run seed || echo "⚠️ Seed script failed, but continuing..."
 
 echo "🚀 Starting the API..."
 exec npm run start
