69c5bfe70843f8d3faa77284502b2ac01c38d41b
Guilhamasse <quentinguilha@gmail.com>
2025-05-07 10:08:17 +0200

    add store for test

--- my_memo_master_front/src/pages/ExercisesPage.vue
+++ my_memo_master_front/src/pages/ExercisesPage.vue
@@ -4,15 +4,15 @@
       <SubjectFilterComponent />
       <div class="flex gap-5 mr-10">
         <button class="flex items-center gap-2 bg-primary rounded-lg py-0 px-5 text-greyCustom">
-          <img class="w-[16px] h-[16px]" :src="editIcon" alt="editIcon">
+          <img class="w-[16px] h-[16px]" :src="editIcon" alt="editIcon" />
           Create test
         </button>
-        <img :src=helpIcon alt="helpIcon">
+        <img :src="helpIcon" alt="helpIcon" />
       </div>
     </section>
 
     <section class="mt-8">
-      <Grid :items="tests">
+      <Grid :items="testStore.tests">
         <template #item="{ item }">
           <Tutorial :tutorial="item" />
         </template>
@@ -22,32 +22,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import SubjectFilterComponent from '@/components/SubjectFilterComponent.vue';
+import { onMounted } from 'vue'
+import { useTestStore } from '@/stores/test' // ✅ Chemin selon ton arborescence
+import SubjectFilterComponent from '@/components/SubjectFilterComponent.vue'
 import Grid from '@/components/GridComponent.vue'
 import Tutorial from '@/components/TutorialItem.vue'
-import helpIcon from'@/assets/test/Help_circle.png';
-import editIcon from '@/assets/test/Edit.png';
+import helpIcon from '@/assets/test/Help_circle.png'
+import editIcon from '@/assets/test/Edit.png'
 
-const tests = ref([
-  {
-    id: 1,
-    title: 'Tutorial 1',
-    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    description: 'Lorem ispsum Lorem isrem ispsumLorem ispsum',
-    tips: false,
-    subject: 1,
-  },
-  {
-    id: 1,
-    title: 'Tutorial 1',
-    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    description: 'Lorem ispsum Lorem isrem ispsumLorem ispsum',
-    tips: true,
-    subject: {
-      name: 'Sujet 1',
-    }
-  },
-])
-  
+const testStore = useTestStore()
+
+onMounted(() => {
+  testStore.fetchTests()
+})
 </script>
