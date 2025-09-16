3bc5c012431ee72f7b4c147edafd1c36c34dfff9
entrezunfredici <82083437+entrezunfredici@users.noreply.github.com>
2025-09-16 22:00:08 +0200

    Fix Role table name for Sequelize sync

--- my_memo_master_api/models/Role.model.js
+++ my_memo_master_api/models/Role.model.js
@@ -1,31 +1,35 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (instance) => {
-    return instance.define('Role', {
-        roleId: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-    }, {
-        tableName: 'roles',
-        updatedAt: 'updatedAt',
-        createdAt: 'createdAt',
-        timestamps: true,
-    })
-}
+  return instance.define(
+    "Role",
+    {
+      roleId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      tableName: "Role",
+      updatedAt: "updatedAt",
+      createdAt: "createdAt",
+      timestamps: true,
+    }
+  );
+};
