8029ff2bd9dda71600cd0429eff002986914644e
entrezunfredici <superfred2468@gmail.com>
2025-11-11 10:33:17 +0100

    [ADD] credits page

---
+++ my_memo_master_front/src/pages/CreditsPage.vue
@@ -0,0 +1,221 @@
+<template>
+    <div>
+        <section id="thank_you" class="flex items-center justify-between">
+            Je tiens a grandement remercier toutes les personnes qui ont travaillé sur My Memo Master, qui ont fournit un excellent travail et qui ont donnéé de leur temps pour créer cet outils qui je l'espère pourra aider nombre d'étudiants dans leur révisions.
+            <br/>
+            - Fred 
+            <table>
+                <ul>
+                    <li>
+                        Prénom
+                    </li>
+                    <li>
+                        Nom
+                    </li>
+                    <li>
+                        Fonction
+                    </li>
+                    <li>
+                        Années
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Clélia
+                    </li>
+                    <li>
+                        Potorel
+                    </li>
+                    <li>
+                        UI/UX designer
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2025
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Anthony
+                    </li>
+                    <li>
+                        Lalba
+                    </li>
+                    <li>
+                        Lead tech, developpeur & associé
+                    </li>
+                    <li>
+                        17/10/2024 - +∞
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Gaia
+                    </li>
+                    <li>
+                        Ducourneau
+                    </li>
+                    <li>
+                        developpeuse
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Jorgeline
+                    </li>
+                    <li>
+                        Ledesma
+                    </li>
+                    <li>
+                        developpeuse
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Nicolas
+                    </li>
+                    <li>
+                        Poda
+                    </li>
+                    <li>
+                        developpeur
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2025
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Quentin
+                    </li>
+                    <li>
+                        Ghilamsse
+                    </li>
+                    <li>
+                        developpeur
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2025
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Jordan
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        developpeur
+                    </li>
+                    <li>
+                        17/10/2024 - 01/07/2025
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Lena
+                    </li>
+                    <li>
+                        Ricard
+                    </li>
+                    <li>
+                        developpeuse
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Ilias
+                    </li>
+                    <li>
+                        Ouandouri
+                    </li>
+                    <li>
+                        developpeur
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Achouak
+                    </li>
+                    <li>
+                        Dairak
+                    </li>
+                    <li>
+                        Experte IA
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Isra
+                    </li>
+                    <li>
+                        Guesmi
+                    </li>
+                    <li>
+                        Experte IA
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Ouassim
+                    </li>
+                    <li>
+                        Djekoun Hocine
+                    </li>
+                    <li>
+                        Expert IA
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Abel
+                    </li>
+                    <li>
+                        Giroud
+                    </li>
+                    <li>
+                        Expert en marketing & communication
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        Valentin
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        Administrateur système & devops
+                    </li>
+                    <li>
+                        29/10/2024 - 01/07/2026
+                    </li>
+                </ul>
+            </table>
+        </section>
+    </div>
+</template>
