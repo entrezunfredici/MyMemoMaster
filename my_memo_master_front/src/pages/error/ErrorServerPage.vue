737396d66b09e93aeeaefd1d413f721a30515eca
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-11-19 11:21:58 +0100

    Misc

--- my_memo_master_front/src/pages/error/ErrorServerPage.vue
+++ my_memo_master_front/src/pages/error/ErrorServerPage.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-96">
-    <p class="text-2xl font-bold text-primary">Une erreur est survenue</p>
+  <div class="flex flex-col items-center justify-center gap-4">
+    <h1 class="text-[8rem] font-bold text-primary">500</h1>
     <p class="text-lg text-dark">Il semble qu'il y ait un problème avec le serveur. Veuillez réessayer plus tard.</p>
-    <router-link to="/" class="text-primary text-lg font-bold underline">Réessayer</router-link>
+    <Button @click="goHome()">Réessayer</Button>
   </div>
 </template>
+
+<script setup>
+import Button from '@/components/ButtonComponent.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goHome() {
+  router.push('/')
+}
+
+</script>
