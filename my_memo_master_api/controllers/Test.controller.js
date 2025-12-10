d030448cc3fd74647ca3d46f30f681b6e413b772
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-12-10 10:34:31 +0100

    [FIX] Correct for MR

--- my_memo_master_api/controllers/Test.controller.js
+++ my_memo_master_api/controllers/Test.controller.js
@@ -8,7 +8,7 @@ exports.findAll = async (req, res) => {
     res.status(500).send({
       message:
         error.message ||
-        "Une erreur s'est produite lors de la récupération des tests.",
+        `Une erreur s'est produite lors de la récupération des tests.`,
     });
   }
 };
@@ -37,7 +37,7 @@ exports.create = async (req, res) => {
     res.status(201).send(data);
   } catch (error) {
     res.status(500).send({
-      message: `Une erreur s'est produite lors de la création du test. ${error.message}`,
+      message: `Une erreur s'est produite lors de la création du test.  ${error.message}`,
     });
   }
 };
@@ -51,7 +51,7 @@ exports.update = async (req, res) => {
     res.status(200).json(updatedTest);
   } catch (error) {
     res.status(500).send({
-      message: `Une erreur inattendue s'est produite lors de la modification du test. Veuillez réessayer plus tard. ${error.message}`,
+      message: error.message || `Une erreur inattendue s'est produite lors de la modification du test. Veuillez réessayer plus tard.`,
     });
   }
 },
