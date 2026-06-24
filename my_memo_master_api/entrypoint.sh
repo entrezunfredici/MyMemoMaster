de34e29e836fbe960f782ee8dde408eb45bec62c
entrezunfredici <superfred2468@gmail.com>
2026-06-24 17:06:13 +0200

    [IMP]

--- my_memo_master_api/entrypoint.sh
+++ my_memo_master_api/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ ! -d node_modules ]; then
-  echo "[entrypoint] Installing dependencies..."
-  npm install
-fi
+echo "[entrypoint] Syncing dependencies..."
+npm install --prefer-offline
 
 echo "[entrypoint] Running database migrations..."
 npx sequelize-cli db:migrate
